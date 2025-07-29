7ae21049d90a58e114022c5d863c66f50e4119a5
princesstiffy <tiffanyrubio78@gmail.com>
2025-07-28 21:44:32 -0400

    animations and transitions

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-// pop in scroll animation
+// fade page transitions using Barba.js 
+
+barba.init({
+  transitions: [
+    {
+      name: 'fade',
+      leave({ current }) {
+        return gsap.to(current.container, {
+          opacity: 0,
+          duration: 0.3, 
+          ease: 'expo.out'
+        });
+      },
+      enter({ next }) {
+        return gsap.from(next.container, {
+          opacity: 0,
+          duration: 0.5, // make fade-in slower
+          ease: 'expo.out' // smoother than linear
+        });
+      }
+    }
+  ]
+});
 
